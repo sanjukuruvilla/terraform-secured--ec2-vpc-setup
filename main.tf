473f7ecd7090a178db9096c850664c6cf2fe1a13
Sanju Kuruvilla <kuruvillasanju@gmail.com>
2024-03-25 23:55:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,5 @@
 # main.tf
 
-# Import variables
-variable "ami_id" {}
-variable "instance_type" {}
-variable "key_name" {}
-
 # Define the VPC
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16" # Define the CIDR block for the VPC
