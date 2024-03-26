4960c433d8c2cdcddd80943372f7d089211c60af
Sanju Kuruvilla <kuruvillasanju@gmail.com>
2024-03-26 15:33:23 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
 # Update the package index and upgrade installed packages
-sudo apt-get update
-sudo apt-get upgrade -y
-
+sudo apt-get update && sudo apt-get upgrade -y
 # Install Ansible
-sudo apt-get install -y ansible
+sudo apt install -y ansible
